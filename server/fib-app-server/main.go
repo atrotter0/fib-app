d4f95fda1ec73891341592409190d477b9deeb89
Abel Trotter <a_trotter0@hotmail.com>
2018-09-11 14:21:00 -0700

    Add go server directory, add items to gitignore

---
+++ server/fib-app-server/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+  "net/http"
+  "github.com/gin-gonic/contrib/static"
+  "github.com/gin-gonic/gin"
+  "github.com/itsjamie/gin-cors"
+)
+
+func main() {
+  // Set the router as the default one shipped with Gin
+	router := gin.Default()
+
+  // Serve frontend static files
+  router.Use(static.Serve("/", static.LocalFile("./views/js", true)))
+  router.Use(cors.Middleware(cors.Config{
+    Origins:        "*",
+    Methods:        "GET, PUT, POST, DELETE",
+    RequestHeaders: "Origin, Authorization, Content-Type",
+    ExposedHeaders: "",
+    Credentials: true,
+    ValidateHeaders: false,
+  }))
+
+  // Setup route group for the API
+  api := router.Group("/api")
+  {
+    api.GET("/", func(c *gin.Context) {
+      c.JSON(http.StatusOK, gin.H {
+        "message": "pong",
+      })
+		})
+  }
+  api.GET("/fibonacci/:num", FibHandler)
+
+	// Start and run the server
+  router.Run(":8080")
+}
+
+func FibHandler(c *gin.Context) {
+  c.Header("Content-Type", "application/json")
+  c.JSON(http.StatusOK, gin.H {
+    "message":"Returning a response",
+  })
+}
