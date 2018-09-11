cf815d49050bfc03d5b6684f7b9acfa143ab5cf6
Abel Trotter <a_trotter0@hotmail.com>
2018-09-11 13:32:47 -0700

    Add conditional rendering to UI

--- src/components/FibGenerator.js
+++ src/components/FibGenerator.js
@@ -4,31 +4,48 @@ import Input from '@material-ui/core/Input';
 import InputLabel from '@material-ui/core/InputLabel';
 import Button from '@material-ui/core/Button';
 
-const FibGenerator = () => {
-  const paperStyles = {
-    width: '50%',
-    maxWidth: '500px',
-    height: '200px',
-    margin: '20px auto',
-    padding: '20px',
-    textAlign: 'center'
+class FibGenerator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      resultsVisible: false
+    }
+    this.handleResultsVisible = this.handleResultsVisible.bind(this);
   }
-  const buttonStyles = {
-    marginTop: '20px'
+  handleResultsVisible = () => {
+    this.setState(() => ({resultsVisible: true}))
   }
-  return(
-    <div>
-      <Paper style={paperStyles}>
-        <InputLabel htmlFor='fib-input'>
-          Enter a number:
-        </InputLabel><br/>
-        <Input type='number' id='fib-input' /><br/>
-        <Button variant='contained' color='primary' style={buttonStyles}>
-          Generate
-        </Button>
-      </Paper>
-    </div>
-  );
+  render() {
+    const paperStyles = {
+      width: '50%',
+      maxWidth: '500px',
+      height: '200px',
+      margin: '20px auto',
+      padding: '20px',
+      textAlign: 'center'
+    }
+    const buttonStyles = {
+      marginTop: '20px'
+    }
+    return(
+      <div>
+        <Paper style={paperStyles}>
+          <InputLabel htmlFor='fib-input'>
+            Enter a number:
+          </InputLabel><br/>
+          <Input type='number' id='fib-input' /><br/>
+          <Button variant='contained' color='primary' style={buttonStyles} onClick={this.handleResultsVisible}>
+            Generate
+          </Button>
+          {this.state.resultsVisible && (
+            <div>
+              <p>Put results here!</p>
+            </div>
+          )}
+        </Paper>
+      </div>
+    );
+  }
 }
 
 export default FibGenerator;
